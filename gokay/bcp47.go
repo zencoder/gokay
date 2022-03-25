128b005e29b60ce57c493ce660d2d2b19e352d73
omartinezbc <51922113+omartinezbc@users.noreply.github.com>
2022-03-25 03:42:50 -0600

    Allow value error for bcp47 language validation (#24)

--- gokay/bcp47.go
+++ gokay/bcp47.go
@@ -9,5 +9,10 @@ func IsBCP47(s *string) error {
 
 	_, err := language.Parse(*s)
 
+	// Pass tags that are well-formed, but not in the spec
+	if _, ok := err.(language.ValueError); ok {
+		return nil
+	}
+
 	return err
 }
